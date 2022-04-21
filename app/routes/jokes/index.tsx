7afff5a2bc000fe1bd3a4a59a7debb0d3314d869
Marek Zelinka <mzelinka17@gmail.com>
2022-04-21 17:04:12 +0200

    setup routes

---
+++ app/routes/jokes/index.tsx
@@ -0,0 +1,10 @@
+export default function JokesIndex() {
+  return (
+    <div>
+      <p>Here's a random joke:</p>
+      <p>
+        I was woundering why the frisbee was getting bigger, then it hit me.
+      </p>
+    </div>
+  );
+}
